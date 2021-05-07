2aae3c64e29e0da559b74a857c1a8e6f048c247a
IJK4041 <44420391+IJK4041@users.noreply.github.com>
2021-05-07 13:56:06 +0800

    分支添加

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -5,6 +5,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("更新1");
         System.out.println("更新创建分支");
-        System.out.println("主干添加");
+        System.out.println("分支添加");
     }
 }
